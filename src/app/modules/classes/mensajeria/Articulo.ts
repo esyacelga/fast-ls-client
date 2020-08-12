e9380889433379679f392b8c1f53a179ba2d3bd4
santiago.yacelga <esyacelga@hotmail.com>
2020-08-12 09:32:57 -0500

    Articulo update

--- src/app/modules/classes/mensajeria/Articulo.ts
+++ src/app/modules/classes/mensajeria/Articulo.ts
@@ -1,29 +1,33 @@
 import {ArticuloSegmento} from './articulo-segmento';
 
 export class Articulo {
-    _id: string;
-    descripcion: string;
-    unidadAlmacenada: number;
-    unidadCosto: number;
-    articuloSegmento: string;
-    estado: number;
-    posicion: false;
-    portada: string;
-    coords: string;
-    imgs?: string[];
+    public _id: string;
+    public descripcion: string;
+    public unidadAlmacenada: number;
+    public unidadCosto: number;
+    public articuloSegmento: string;
+    public estado: number;
+    public posicion: false;
+    public portada: string;
+    public coords: string;
+    public esServicio: false;
+    public permiteComentar: false;
+    public imgs?: string[];
 }
 
 
 export class ObjetoArticulo {
-    _id: string;
-    descripcion: string;
-    unidadAlmacenada: number;
-    unidadCosto: number;
-    articuloSegmento: ArticuloSegmento = new ArticuloSegmento();
-    estado: number;
-    posicion: false;
-    portada: string;
-    imagenEditada: string;
-    coords: string;
-    imgs?: string[];
+    public _id: string;
+    public descripcion: string;
+    public unidadAlmacenada: number;
+    public unidadCosto: number;
+    public articuloSegmento: ArticuloSegmento = new ArticuloSegmento();
+    public estado: number;
+    public posicion: false;
+    public portada: string;
+    public imagenEditada: string;
+    public esServicio: false;
+    public permiteComentar: false;
+    public coords: string;
+    public imgs?: string[];
 }
